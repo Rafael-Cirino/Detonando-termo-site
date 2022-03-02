6595b29e89ebd06c86437021635b693b6ff238f5
Rafael-Cirino <r223730@dac.unicamp.br>
2022-03-01 22:24:50 -0300

    Upando código js+html+css do detonando-termo

---
+++ scripts/main.js
@@ -0,0 +1,140 @@
+var log = document.getElementById("log")
+var tentativa = -1
+var allText, list_words
+var word_partial = "01234"
+var list_in = []
+var list_in_id = []
+var list_block = []
+
+function buttons() {
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 5; j++) {
+            let btn = document.createElement("button")
+            let text_box = `${i}${j}`
+            let text_id = `id:${i}:${j}`
+
+            btn.setAttribute("class", "box")
+            btn.disabled = true
+
+            btn.setAttribute("value", text_box)
+            btn.textContent = text_box
+
+            btn.setAttribute("id", text_id)
+            btn.setAttribute("name", text_id)
+            btn.style.top = `${18 + i * 9.8}%`
+            btn.style.left = `${38 + j * 4.8}%`
+
+            btn.setAttribute('onclick', 'color_box("' + text_id + '")')
+            document.body.appendChild(btn)
+        }
+    }
+
+    let list_b = ["TROCA PALAVRA", "ENTER"]
+    for (var b_name in list_b) {
+        let btn = document.createElement("button")
+        let text_box = list_b[b_name]
+        let text_id = list_b[b_name]
+
+        btn.setAttribute("class", "box enter")
+        btn.setAttribute("value", text_box)
+        btn.textContent = text_box
+
+        btn.setAttribute("id", text_id)
+
+        if (list_b[b_name] == "ENTER") {
+            btn.setAttribute('onclick', "enter_main()")
+            btn.style.left = `${53}%`
+        } else {
+            btn.style.left = `${38}%`
+            btn.setAttribute('onclick', 'random_word()')
+        }
+        document.body.appendChild(btn)
+    }
+}
+
+function color_box(id_box) {
+    let box = document.getElementById(id_box)
+
+    if (["edit", "wrong"].includes(box.classList[1])) {
+        box.setAttribute("class", "box yellow")
+    } else if (box.classList[1] == "yellow") {
+        box.setAttribute("class", "box right")
+    } else {
+        box.setAttribute("class", "box wrong")
+    }
+}
+
+function writeBox(word, i) {
+    for (var letra in word) {
+        let id_box = `id:${i}:${letra}`
+        let box = document.getElementById(id_box)
+
+        box.setAttribute("class", "box edit")
+        box.setAttribute("value", word[letra])
+        box.textContent = word[letra]
+        box.disabled = false
+    }
+
+}
+
+function closeBox(i) {
+    for (var j = 0; j < 5; j++) {
+        let id_box = `id:${i}:${j}`
+        let box = document.getElementById(id_box)
+        let box_class = box.classList[1]
+        let box_letter = box.value.toLowerCase()
+
+        let letra_in = `${box_letter}:${j}`
+        if ((box_class == "edit")) {
+            box.setAttribute("class", "box wrong")
+            if (!list_block.includes(box_letter) && !list_in.includes(box_letter) && !word_partial.includes(box_letter)) {
+                list_block.push(box_letter)
+            }
+        } else if ((box_class == "yellow") && !list_in_id.includes(letra_in)) {
+            list_in_id.push(letra_in)
+
+            if (!list_in.includes(box_letter)) {
+                list_in.push(box_letter)
+            }
+        }
+        else if (box_class == "right") {
+            word_partial = word_partial.replace(String(j), box_letter)
+        }
+
+        box.disabled = true
+    }
+}
+
+function random_word() {
+    if (tentativa == 1) {
+        word = list_words[Math.floor(Math.random() * list_words.length)]
+        writeBox(word, 0)
+    } else {
+        word = list_words[Math.floor(Math.random() * list_words.length)]
+        writeBox(word, tentativa - 1)
+    }
+
+}
+
+function enter_main() {
+    var word = "BARCO"
+
+    if (tentativa == -1) {
+        writeBox(word, 0)
+        list_words = readjson("./json_words.json")
+
+        tentativa = 1
+    } else if (tentativa <= 5) {
+        closeBox(tentativa - 1)
+
+        list_words = found_words(list_words, list_in, list_in_id, list_block, word_partial)
+        word = list_words[Math.floor(Math.random() * list_words.length)]
+        writeBox(word, tentativa)
+
+        log.innerHTML = "<p>Banco de palavras:</p>"
+        log.innerHTML += `<p>${list_words.length}</p>`
+
+        tentativa += 1
+    }
+
+}
---
+++ scripts/termo_func.js
@@ -0,0 +1,120 @@
+var allText
+
+function readjson(path_json) {
+    readTextFile(path_json)
+    allText = JSON.parse(allText)
+    return allText
+}
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                allText = rawFile.responseText;
+                //alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function create_regex_or(list_or, condition) {
+    let word_regex_in = ""
+
+    if (condition == "or") {
+        word_regex_in = `(`
+
+        for (id in list_or) {
+            if (id == (list_or.length - 1)) {
+                word_regex_in += `${list_or[id]})`
+                break
+            }
+
+            word_regex_in += `${list_or[id]}|`
+        }
+    } else {
+        for (id in list_or) {
+            word_regex_in += `(?=.*${list_or[id]})`
+        }
+    }
+
+    //console.log(word_regex_in)
+    return word_regex_in
+}
+
+function regex_list(reg, list_words) {
+    // Regex
+    return list_words.filter(function (word) {
+        return reg.test(word)
+    })
+}
+
+function remove_item(list_remove, list_words) {
+    list_remove.filter(function (word) {
+        let index = list_words.indexOf(word)
+        list_words.splice(index, 1)
+    })
+
+    return list_words
+}
+
+function found_words(list_words, list_in, list_in_id, list_block, word_partial) {
+    let reg = ""
+
+    if (true) {
+        for (id in word_partial) {
+            if (!isNaN(word_partial[id])) {
+                reg += "[a-z]{1}"
+            } else {
+                reg += word_partial[id]
+            }
+        }
+
+        list_words = regex_list(RegExp(reg), list_words)
+    }
+
+
+
+    if (list_block.length > 0) {
+        reg = create_regex_or(list_block, "or")
+        list_remove = regex_list(RegExp(reg), list_words)
+
+        list_words = remove_item(list_remove, list_words)
+
+        //console.log(list_remove)
+    }
+
+    if (list_in.length > 0) {
+        //Letra sem posição
+        reg = create_regex_or(list_in, "and")
+        list_words = regex_list(RegExp(reg), list_words)
+
+        //Letra com posição
+        let list_remove = []
+        for (palavra in list_words) {
+            for (id in list_in_id) {
+                let aux = list_in_id[id].split(":")
+
+                if (list_words[palavra].includes(aux[0])) {
+                    for (l_id in list_words[palavra]) {
+                        console.log(list_words[palavra][l_id], aux[0], aux[1])
+                        if ((list_words[palavra][l_id] == aux[0]) && (l_id == Number.parseInt(aux[1]))) {
+                            list_remove.push(list_words[palavra])
+                            break
+                        }
+                    }
+                }
+            }
+        }
+
+        list_words = remove_item(list_remove, list_words)
+        //console.log(list_in_id)
+        //console.log(list_remove)
+    }
+
+    console.log(list_words)
+
+    return list_words
+}
