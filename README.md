5554cfe7c55c0b7e79bb9695001e4545f74968ad
Rafael-Cirino <r223730@dac.unicamp.br>
2022-03-08 19:28:20 -0300

    Arrumando visualização mobile

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Detonando-termo-site
-Código que resolve os jogos de palavra Letreco e Termo AGORA EM SITE!
+    Código que resolve os jogos de palavra Letreco e Termo AGORA EM SITE!
 
 
 [Projeto Python](https://github.com/Rafael-Cirino/Detonando-letreco-termo)
--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ function buttons() {
             //btn.style.top = `${55 + (i + 1) * 50}px`
             //btn.style.left = `${20 + (j + 1) * 50}px`
             if (window.innerWidth <= 641) {
-                btn.style.top = `${6 + (i + 1) * 8}vh`
+                btn.style.top = `${95 + (i + 1) * 8}vh`
                 btn.style.left = `${(j + 0.8) * 16}vw`
             } else {
                 btn.style.top = `max(${4 + (i + 1) * 4.8}%, ${3 + (i + 1) * 4.8}vw)`
