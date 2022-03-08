2b1678a508ba7b629487130d008e4f3d0bfebb45
Rafael-Cirino <r223730@dac.unicamp.br>
2022-03-08 11:26:12 -0300

    Quarteto está pronto

--- scripts/quarteto.js
+++ scripts/quarteto.js
@@ -1,10 +1,13 @@
 var tentativa = -1
+var c_break = [0, 1, 2, 3]
 var allText
 var word_partial = ["01234", "01234", "01234", "01234"]
 var list_in = [[], [], [], []]
 var list_in_id = [[], [], [], []]
 var list_block = [[], [], [], []]
 var list_words = []
+
+//Resolver o b.o que não deixa travar as colunas
 
 function buttons_4() {
     for (var k = 0; k < 4; k++) {
@@ -142,7 +145,7 @@ function closeBox(i) {
 }
 
 function writeBox_4(word, i) {
-    for (var k = 0; k < 4; k++) {
+    for (let k of c_break) {
         for (var letra in word) {
             let id_box = `id:${k}:${i}:${letra}`
             let box = document.getElementById(id_box)
@@ -155,29 +158,58 @@ function writeBox_4(word, i) {
     }
 }
 
+function word_rigth(word_aux) {
+    let reg
+
+    for (id in word_aux) {
+        if (!isNaN(word_aux[id])) {
+            return false
+        } else {
+            reg = true
+        }
+    }
+
+    return reg
+}
+
 function word_select() {
-    let word
+    let word = "XXXXX"
     let id_sel = 0
     let aux = 10 ** 5
+    let list_remove = []
 
-    for (let k = 0; k < 3; k++) {
-        if ((list_words[k].length < aux) && (list_words[k].length != 0)) {
+
+    for (let k of c_break) {
+        let list_len = list_words[k].length
+
+        console.log(list_len)
+        if (word_rigth(word_partial[k])) {
+            console.log(list_len, "passou aqui")
+            list_remove.push(k)
+        } else if ((list_len < aux) && (list_len > 0)) {
             id_sel = k
             aux = list_words[k].length
         }
     }
 
-    if (list_words[id_sel].length <= 1) {
+    for (let k of list_remove) {
+        if (c_break.indexOf(k) > -1) {
+            c_break.splice(c_break.indexOf(k), 1)
+        }
+    }
+
+    console.log(aux, "aux select")
+    if (aux == 1) {
         return list_words[id_sel][0]
     }
 
     let i = 0
     let point = 0
-    while (i <= 10) {
+    while (i <= 20) {
         let point_aux = 0
-        word = list_words[id_sel][Math.floor(Math.random() * list_words.length)]
+        word = list_words[id_sel][Math.floor(Math.random() * list_words[id_sel].length)]
 
-        for (let k = 0; k < 4; k++) {
+        for (let k of c_break) {
             if ([word].includes(list_words[k])) {
                 point_aux += 1
             }
@@ -185,13 +217,13 @@ function word_select() {
 
         if (point_aux > point) {
             point = point_aux
-        } else if (point_aux == 4) {
+        } else if (point_aux >= 2) {
+            console.log(word)
             return word
         }
         i += 1
     }
 
-    console.log(id_sel)
     return word
 }
 
@@ -204,6 +236,10 @@ function enter_main_4() {
         for (var k = 0; k < 4; k++) {
             let list_aux = readjson("./json_words.json")
             list_words.push(list_aux)
+
+            let log = document.getElementById(`div_log_${k}`)
+            log.innerHTML = "<p>Banco de palavras:</p>"
+            log.innerHTML += `<p>${list_words[k].length}</p>`
         }
 
         console.log(list_words)
@@ -213,7 +249,7 @@ function enter_main_4() {
     } else if (tentativa <= 9) {
         closeBox(tentativa - 1)
 
-        for (var k = 0; k < 4; k++) {
+        for (let k of c_break) {
             list_words[k] = found_words(list_words[k], list_in[k], list_in_id[k], list_block[k], word_partial[k])
 
             let log = document.getElementById(`div_log_${k}`)
